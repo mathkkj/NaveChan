2ee33afbf7a78dfd26dc01c577a1443ef000fc75
mathkkj <matheus.cabral24@nave.org.br>
2026-04-25 15:45:46 -0300

    update 1

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+
+
 const supabase = createClient(
   "https://prffblgjpmyhspazhhvr.supabase.co",
   "sb_publishable_2dPZBezbL79SlLaDJIkKtA_HNI6AIIW"
 );
 
 async function carregarPosts() {
-  const { data } = await supabase
+  const { data } = await supabase //lembrar de colocar o await supabase toda hora antes de carregar algo
     .from("posts")
     .select("*")
     .order("created_at", { ascending: false });
@@ -14,6 +16,7 @@ async function carregarPosts() {
   const div = document.getElementById("posts");
   div.innerHTML = "";
 
+  // aqui q coloca os post com foreach
   data.forEach((post) => {
     const el = document.createElement("div");
     el.className = "post";
@@ -32,12 +35,12 @@ async function enviarPost() {
   const input = document.getElementById("input");
   const texto = input.value.trim();
 
-  if (!texto) return;
+  if (!texto) return; //pra nao bugar caso n tenha texto
 
-  await supabase.from("posts").insert([{ conteudo: texto }]);
+  await supabase.from("posts").insert([{ conteudo: texto }]); //esperar o supabase carregar
 
   input.value = "";
-  carregarPosts();
+  carregarPosts(); 
 }
 
 document.getElementById("btnPostar").addEventListener("click", enviarPost);
